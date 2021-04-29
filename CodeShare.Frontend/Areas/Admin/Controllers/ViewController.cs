98e98c57e0469959a7a0152a10c8899121100e49
trantran7154 <54873044+trantran7154@users.noreply.github.com>
2021-04-29 15:16:19 +0700

    static Admin

---
+++ CodeShare.Frontend/Areas/Admin/Controllers/ViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeShare.Frontend.Areas.Admin.Controllers
+{
+    public class ViewController : Controller
+    {
+        // GET: Admin/View
+        public PartialViewResult Statistical()
+        {
+            return PartialView();
+        }
+    }
+}
